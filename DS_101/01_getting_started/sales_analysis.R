6a96d3563df8dbe734af924ac4de6834ebdbe7b4
ttsagor <ttsagor@gmail.com>
2020-11-25 02:40:11 +0100

    chapter 3

--- DS_101/01_getting_started/sales_analysis.R
+++ DS_101/01_getting_started/sales_analysis.R
@@ -5,6 +5,8 @@
 library(tidyverse)
 library(readxl)
 library(lubridate)
+library(writexl)
+library(lubridate)
 # 2.0 Importing Files ----
 bikes_tbl      <- read_excel(path = "C:/Users/sagor/Documents/GitHub/ws20-business-data-science-basics---lab-journal-ttsagor/DS_101/00_data/01_bike_sales/01_raw_data/bikes.xlsx")
 orderlines_tbl <- read_excel("C:/Users/sagor/Documents/GitHub/ws20-business-data-science-basics---lab-journal-ttsagor/DS_101/00_data/01_bike_sales/01_raw_data/orderlines.xlsx")
@@ -76,119 +78,119 @@ bike_orderlines_wrangled_tbl <- bike_orderlines_joined_tbl %>%
   rename(bikeshop = name) %>%
   set_names(names(.) %>% str_replace_all("\\.", "_"))
 
-bike_orderlines_wrangled_tbl
-# 6.0 Business Insights ----
-# 6.1 Sales by Year ----
-
-# Step 1 - Manipulate
-library(lubridate)
-sales_by_year_tbl <- bike_orderlines_wrangled_tbl %>%
-  
-  # Select columns
-  select(order_date, total_price) %>%
-  
-  # Add year column
-  mutate(year = year(order_date)) %>%
-  
-  # Grouping by year and summarizing sales
-  group_by(year) %>% 
-  summarize(sales = sum(total_price)) %>%
-  
-  # Optional: Add a column that turns the numbers into a currency format 
-  # (makes it in the plot optically more appealing)
-  # mutate(sales_text = scales::dollar(sales)) <- Works for dollar values
-  mutate(sales_text = scales::dollar(sales, big.mark = ".", 
-                                     decimal.mark = ",", 
-                                     prefix = "", 
-                                     suffix = " €"))
-
-sales_by_year_tbl
-# Step 2 - Visualize
-sales_by_year_tbl %>%
-  
-  # Setup canvas with the columns year (x-axis) and sales (y-axis)
-  ggplot(aes(x = year, y = sales)) +
-  
-  # Geometries
-  geom_col(fill = "#2DC6D6") + # Use geom_col for a bar plot
-  geom_label(aes(label = sales_text)) + # Adding labels to the bars
-  geom_smooth(method = "lm", se = FALSE) + # Adding a trendline
-  
-  # Formatting
-  # scale_y_continuous(labels = scales::dollar) + # Change the y-axis. 
-  # Again, we have to adjust it for euro values
-  scale_y_continuous(labels = scales::dollar_format(big.mark = ".", 
-                                                    decimal.mark = ",", 
-                                                    prefix = "", 
-                                                    suffix = " €")) +
-  labs(
-    title    = "Revenue by year",
-    subtitle = "Upward Trend",
-    x = "", # Override defaults for x and y
-    y = "Revenue"
-  )
-
-# 6.2 Sales by Year and Category 2 ----
-
-# Step 1 - Manipulate
-sales_by_year_cat_1_tbl <- bike_orderlines_wrangled_tbl %>%
-  
-  # Select columns and add a year
-  select(order_date, total_price, category_1) %>%
-  mutate(year = year(order_date)) %>%
-  
-  # Group by and summarize year and main catgegory
-  group_by(year, category_1) %>%
-  summarise(sales = sum(total_price)) %>%
-  ungroup() %>%
-  
-  # Format $ Text
-  mutate(sales_text = scales::dollar(sales, big.mark = ".", 
-                                     decimal.mark = ",", 
-                                     prefix = "", 
-                                     suffix = " €"))
-
-sales_by_year_cat_1_tbl  
-
-# Step 2 - Visualize
-
-# Step 2 - Visualize
-sales_by_year_cat_1_tbl %>%
-  
-  # Set up x, y, fill
-  ggplot(aes(x = year, y = sales, fill = category_1)) +
-  
-  # Geometries
-  geom_col() + # Run up to here to get a stacked bar plot
-  
-  # Facet
-  facet_wrap(~ category_1) +
-  
-  # Formatting
-  scale_y_continuous(labels = scales::dollar_format(big.mark = ".", 
-                                                    decimal.mark = ",", 
-                                                    prefix = "", 
-                                                    suffix = " €")) +
-  labs(
-    title = "Revenue by year and main category",
-    subtitle = "Each product category has an upward trend",
-    fill = "Main category" # Changes the legend name
-  )
-
-# 7.0 Writing Files ----
-
-# 7.1 Excel ----
-library("writexl")
-bike_orderlines_wrangled_tbl %>%
-  write_xlsx("C:/Users/sagor/Documents/GitHub/ws20-business-data-science-basics---lab-journal-ttsagor/DS_101/00_data/01_bike_sales/02_wrangled_data/bike_orderlines.xlsx")
-# 7.2 CSV ----
-bike_orderlines_wrangled_tbl %>% 
-  write_csv("C:/Users/sagor/Documents/GitHub/ws20-business-data-science-basics---lab-journal-ttsagor/DS_101/00_data/01_bike_sales/02_wrangled_data/bike_orderlines.csv")
-
-# 7.3 RDS ----
-bike_orderlines_wrangled_tbl %>% 
-  write_rds("C:/Users/sagor/Documents/GitHub/ws20-business-data-science-basics---lab-journal-ttsagor/DS_101/00_data/01_bike_sales/02_wrangled_data/bike_orderlines.rds")
-
+# bike_orderlines_wrangled_tbl
+# # 6.0 Business Insights ----
+# # 6.1 Sales by Year ----
+# 
+# # Step 1 - Manipulate
+# 
+# sales_by_year_tbl <- bike_orderlines_wrangled_tbl %>%
+#   
+#   # Select columns
+#   select(order_date, total_price) %>%
+#   
+#   # Add year column
+#   mutate(year = year(order_date)) %>%
+#   
+#   # Grouping by year and summarizing sales
+#   group_by(year) %>% 
+#   summarize(sales = sum(total_price)) %>%
+#   
+#   # Optional: Add a column that turns the numbers into a currency format 
+#   # (makes it in the plot optically more appealing)
+#   # mutate(sales_text = scales::dollar(sales)) <- Works for dollar values
+#   mutate(sales_text = scales::dollar(sales, big.mark = ".", 
+#                                      decimal.mark = ",", 
+#                                      prefix = "", 
+#                                      suffix = " €"))
+# 
+# sales_by_year_tbl
+# # Step 2 - Visualize
+# sales_by_year_tbl %>%
+#   
+#   # Setup canvas with the columns year (x-axis) and sales (y-axis)
+#   ggplot(aes(x = year, y = sales)) +
+#   
+#   # Geometries
+#   geom_col(fill = "#2DC6D6") + # Use geom_col for a bar plot
+#   geom_label(aes(label = sales_text)) + # Adding labels to the bars
+#   geom_smooth(method = "lm", se = FALSE) + # Adding a trendline
+#   
+#   # Formatting
+#   # scale_y_continuous(labels = scales::dollar) + # Change the y-axis. 
+#   # Again, we have to adjust it for euro values
+#   scale_y_continuous(labels = scales::dollar_format(big.mark = ".", 
+#                                                     decimal.mark = ",", 
+#                                                     prefix = "", 
+#                                                     suffix = " €")) +
+#   labs(
+#     title    = "Revenue by year",
+#     subtitle = "Upward Trend",
+#     x = "", # Override defaults for x and y
+#     y = "Revenue"
+#   )
+# 
+# # 6.2 Sales by Year and Category 2 ----
+# 
+# # Step 1 - Manipulate
+# sales_by_year_cat_1_tbl <- bike_orderlines_wrangled_tbl %>%
+#   
+#   # Select columns and add a year
+#   select(order_date, total_price, category_1) %>%
+#   mutate(year = year(order_date)) %>%
+#   
+#   # Group by and summarize year and main catgegory
+#   group_by(year, category_1) %>%
+#   summarise(sales = sum(total_price)) %>%
+#   ungroup() %>%
+#   
+#   # Format $ Text
+#   mutate(sales_text = scales::dollar(sales, big.mark = ".", 
+#                                      decimal.mark = ",", 
+#                                      prefix = "", 
+#                                      suffix = " €"))
+# 
+# sales_by_year_cat_1_tbl  
+# 
+# # Step 2 - Visualize
+# 
+# # Step 2 - Visualize
+# sales_by_year_cat_1_tbl %>%
+#   
+#   # Set up x, y, fill
+#   ggplot(aes(x = year, y = sales, fill = category_1)) +
+#   
+#   # Geometries
+#   geom_col() + # Run up to here to get a stacked bar plot
+#   
+#   # Facet
+#   facet_wrap(~ category_1) +
+#   
+#   # Formatting
+#   scale_y_continuous(labels = scales::dollar_format(big.mark = ".", 
+#                                                     decimal.mark = ",", 
+#                                                     prefix = "", 
+#                                                     suffix = " €")) +
+#   labs(
+#     title = "Revenue by year and main category",
+#     subtitle = "Each product category has an upward trend",
+#     fill = "Main category" # Changes the legend name
+#   )
+# 
+# # 7.0 Writing Files ----
+# 
+# # 7.1 Excel ----
+# 
+# bike_orderlines_wrangled_tbl %>%
+#   write_xlsx("C:/Users/sagor/Documents/GitHub/ws20-business-data-science-basics---lab-journal-ttsagor/DS_101/00_data/01_bike_sales/02_wrangled_data/bike_orderlines.xlsx")
+# # 7.2 CSV ----
+# bike_orderlines_wrangled_tbl %>% 
+#   write_csv("C:/Users/sagor/Documents/GitHub/ws20-business-data-science-basics---lab-journal-ttsagor/DS_101/00_data/01_bike_sales/02_wrangled_data/bike_orderlines.csv")
+# 
+# # 7.3 RDS ----
+# bike_orderlines_wrangled_tbl %>% 
+#   write_rds("C:/Users/sagor/Documents/GitHub/ws20-business-data-science-basics---lab-journal-ttsagor/DS_101/00_data/01_bike_sales/02_wrangled_data/bike_orderlines.rds")
+# 
 
 
 #Challenge 1
